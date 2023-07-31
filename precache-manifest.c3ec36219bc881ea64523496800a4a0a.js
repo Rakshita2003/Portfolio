7905a69925717897ec395e17612a97bbf1b6bf21
Rakshita <rakshitagowda22@gmail.com>
2023-07-31 20:46:18 +0530

    Updates

--- precache-manifest.c3ec36219bc881ea64523496800a4a0a.js
+++ precache-manifest.c3ec36219bc881ea64523496800a4a0a.js
@@ -64,24 +64,24 @@ self.__precacheManifest = [
     "url": "/Portfolio/static/media/contact.51c34c80.png"
   },
   {
-    "revision": "af7ae505a9eed503f8b8e6982036873e",
-    "url": "/Portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
-  },
-  {
     "revision": "674f50d287a8c48dc19ba404d20fe713",
     "url": "/Portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
   },
   {
     "revision": "fee66e712a8a08eef5805a46892932ad",
     "url": "/Portfolio/static/media/fontawesome-webfont.fee66e71.woff"
   },
   {
-    "revision": "912ec66d7572ff821749319396470bde",
-    "url": "/Portfolio/static/media/fontawesome-webfont.912ec66d.svg"
-  },
-  {
     "revision": "b06871f281fee6b241d60582ae9369b9",
     "url": "/Portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Portfolio/static/media/fontawesome-webfont.912ec66d.svg"
   },
   {
     "revision": "5801d30b7de33f79305583d2fdff8933",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.a88ca20b3db55aa05a57e9036095c8a8.js"
+  "/Portfolio/precache-manifest.c3ec36219bc881ea64523496800a4a0a.js"
 );
 
 workbox.clientsClaim();
